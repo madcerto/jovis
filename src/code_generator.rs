de4c8e8703358da2af6aa3e3d3d62447fa33efb3
MadScientist2854 <madscientist2854@gmail.com>
2021-08-04 18:40:33 +0500

    used linker in code generator

--- src/code_generator.rs
+++ src/code_generator.rs
@@ -1,8 +1,13 @@
+use std::ffi::CString;
+use crate::linker::j_link;
 use super::expr::{Expr, compiler::{TypeCheck}};
 
-pub fn generate_code(mut ast: Expr, out_file: String) {
+pub fn _generate_code(mut ast: Expr, out_file: String) {
     ast.check_new_env().unwrap();
     // TODO: Generate IR
     // TODO: Write IR to file
-    // TODO: call linker on IR file
+    // call linker on IR file
+    unsafe {
+        j_link(CString::new(out_file).unwrap().as_ptr());
+    }
 }
